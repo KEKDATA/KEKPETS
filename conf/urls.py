a11b2e868c6278aab470b3a92eaddfcd3be2009f
TchernyavskyDaniil <tchernyavsky.daniil@yandex.ru>
2021-11-02 12:36:45 +0300

    fix: env

--- conf/urls.py
+++ conf/urls.py
@@ -33,7 +33,7 @@ def kek(request, *args, **kwargs):
     query = {
         "match_all": {}
     }
-    r = DetectedObjectDocument.search().query('script_score', script=script, query=query, size=228)
+    r = DetectedObjectDocument.search().query('script_score', script=script, query=query)
     print(r.to_queryset())
     return HttpResponse('KEK')
 
