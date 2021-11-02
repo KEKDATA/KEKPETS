e0558f3107842af21ad9a9c5a60bf88d268f0b98
TchernyavskyDaniil <tchernyavsky.daniil@yandex.ru>
2021-11-02 16:48:20 +0300

    feat: kek size

--- conf/urls.py
+++ conf/urls.py
@@ -33,7 +33,7 @@ def kek(request, *args, **kwargs):
     query = {
         "match_all": {}
     }
-    r = DetectedObjectDocument.search().query('script_score', script=script, query=query)[:1488]
+    r = DetectedObjectDocument.search()[:1488].query('script_score', script=script, query=query)
     print(r.to_queryset())
     return HttpResponse('KEK')
 
