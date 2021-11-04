485c361e2f03e413e67dd7cf951fdb28fffa7905
s-andrew <anso2393@yandex.ru>
2021-11-04 17:13:00 +0300

    fix entrypoint and volumes

--- conf/settings.py
+++ conf/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '.data/db.sqlite3',
     }
 }
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ while ! nc -z elasticsearch 9200; do
   sleep 0.1
 done
 
-echo "PostgreSQL started"
+echo "Elasticsearch started"
 
 
 # flush удаляет все данные из бд
@@ -17,7 +17,7 @@ echo "PostgreSQL started"
 python manage.py collectstatic --noinput --clear
 python manage.py migrate
 python manage.py fill_adresses
-python manage.py create_default_superuser
+python manage.py create_admin
 python manage.py search_index --rebuild -f
 
 exec "$@"
