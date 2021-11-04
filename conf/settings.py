5eff28e6003506adaf2a630f8b4a070ced72bbf6
TchernyavskyDaniil <tchernyavsky.daniil@yandex.ru>
2021-11-04 18:31:10 +0300

    fix: db path

--- conf/settings.py
+++ conf/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '.data/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
